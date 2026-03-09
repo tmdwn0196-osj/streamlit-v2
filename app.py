23a913bb3d772ba15219193a20208ad44b12ac14
tmdwn0196-osj <tmdwn0196@gmail.com>
2026-03-09 12:28:15 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="스마트 홈 대시보드 - 오승주",
+    page_icon="🐶",
+    layout= "centered"
+
+)
+
+st.header("스트림릿 배포 테스트중")
+st.write("스트림릿 배포해보기 - 오승주")
+
+#git add . <- ignore 제외 모든 파일
